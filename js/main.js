79d4201b1e3a9f4303b68bdb699f0bfbb3325984
Leonel Zalazar <leonelzalazar07@gmail.com>
2023-04-26 23:05:41 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Swiper
+var swiper = new Swiper(".bike-slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    // On click autoplay don't stop if false
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    510: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    758: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
